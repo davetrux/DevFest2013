a4e0e3102ed9aa44f8ccd8c6fdd525d7306ec4d2
David Truxall <davetrux@gmail.com>
2013-03-10 15:41:44 -0400

    Initial Project Creation

---
+++ ServiceConsumer/src/com/devfest/serviceconsumer/WebHelper.java
@@ -0,0 +1,28 @@
+package com.devfest.serviceconsumer;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class WebHelper {
+    HttpClient _client = new DefaultHttpClient();
+
+    public String getHttp(String url) throws IOException {
+
+        HttpGet request = new HttpGet(url);
+
+        HttpResponse response = _client.execute(request);
+
+        InputStream stream = response.getEntity().getContent();
+     	byte byteArray[] = IOUtils.toByteArray(stream);
+     	String json = new String( byteArray );
+     	stream.close();
+        return json;
+    }
+}
