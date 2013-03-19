70d3b53e251c9efe1da3dfca1866da7adb2aae4c
David Truxall <davetrux@gmail.com>
2013-03-18 21:14:25 -0400

    Initial Add

---
+++ Starter/src/com/devfest/serviceconsumer/Person.java
@@ -0,0 +1,43 @@
+package com.devfest.serviceconsumer;
+
+public class Person {
+
+    private String mFirstName;
+    
+    private String mLastName;
+
+    private String mGender;
+
+
+    /*
+     * Default constructor for general object creation
+     */
+    public Person() {
+
+    }
+
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.mFirstName = firstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.mLastName = lastName;
+    }
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public void setGender(String gender) {
+        this.mGender = gender;
+    }
+}
---
+++ Starter/src/com/devfest/serviceconsumer/WebHelper.java
@@ -0,0 +1,29 @@
+package com.devfest.serviceconsumer;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class WebHelper {
+    AbstractHttpClient mClient = new DefaultHttpClient();
+
+    public String getHttp(String url) throws IOException {
+
+        //Set up the HTTP calls
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = mClient.execute(request);
+
+        //Get the data from the body of the response
+        InputStream stream = response.getEntity().getContent();
+     	byte byteArray[] = IOUtils.toByteArray(stream);
+     	String json = new String( byteArray );
+     	stream.close();
+        return json;
+    }
+}
